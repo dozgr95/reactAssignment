3707d4434e929aecd82951b2be9e092e5a049924
dozgr95 <dominik.zgraggen@hotmail.com>
2020-12-09 09:13:27 +0100

    add sample async auto complete

--- react-assignment/src/App.js
+++ react-assignment/src/App.js
@@ -1,18 +1,11 @@
 import './App.css';
-import TextField from '@material-ui/core/TextField';
-import Autocomplete from '@material-ui/lab/Autocomplete';
+import AsyncAutoComplete from './AsyncAutoComplete'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <Autocomplete
-        id="account-search"
-        options={accounts}
-        getOptionLabel={(option) => option.title}
-        style={{ width: 300 }}
-        renderInput={(params) => <TextField {...params} label="Combo box" variant="outlined" />}
-      />
+        <AsyncAutoComplete />
       </header>
     </div>
   );
